b55b621f448e6891067d7eab062df987f3a05587
Haripranav <haripranav@Haripranavs-MacBook-Air.local>
2024-07-31 23:54:52 +0530

    chore: webhook cleanup

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,12 +1,14 @@
-import prisma from '@/common/database'
-import { UserPayload } from '@/validators/user.validator'
+import prisma from '@/common/database';
+import { UserPayload } from '@/validators/user.validator';
 
 export class UserModel {
-    static async createUser(user: UserPayload) {
-        return await prisma.user.create({
-            data: { userId: user.userId },
+    static createUser(user: UserPayload) {
+        return prisma.user.upsert({
+            update: {},
+            where: { userId: user.userId },
+            create: { userId: user.userId },
             select: { userId: true },
-        })
+        });
     }
 
     static async archiveUser(user: UserPayload) {
@@ -14,6 +16,6 @@ export class UserModel {
             where: { userId: user.userId },
             data: { isArchived: true },
             select: { userId: true, isArchived: true },
-        })
+        });
     }
 }
